531dd22e3c0a36a10cb1ecab6dc6a50e42d7ee4d
Lina Ismail <ilina19@hotmail.com>
2023-01-22 04:55:58 -0500

    uncommented clock comment

--- frontend/src/Components/Clock.js
+++ frontend/src/Components/Clock.js
@@ -100,50 +100,49 @@ function Clock() {
             });
             setCounter(prevCounter => {
                     
-                //     const input = UserData[prevCounter];
-                //     const cancelInput = UserData[prevCounter];
+                    const input = UserData[prevCounter];
+                    const cancelInput = UserData[prevCounter];
 
-                //     const newData = {
-                //         labels: [],
-                //         datasets: [{
-                //             label: 'Transactions Completed',
-                //             backgroundColor: ["#765dd9", "#866ee6", "#8f78eb", "#9d88f2", "#a590f5", "#b29ffc", "#bcabff", "#c8baff", "#d4c9ff", "#dad2fc"],
-                //             borderColor: 'rgba(255,99,132,1)',
-                //             borderWidth: 1,
-                //             hoverBackgroundColor: 'rgba(255,99,132,0.4)',
-                //             hoverBorderColor: 'rgba(255,99,132,1)',
-                //             data: []
-                //         }]
-                //       }
+                    const newData = {
+                        labels: [],
+                        datasets: [{
+                            label: 'Transactions Completed',
+                            backgroundColor: ["#765dd9", "#866ee6", "#8f78eb", "#9d88f2", "#a590f5", "#b29ffc", "#bcabff", "#c8baff", "#d4c9ff", "#dad2fc"],
+                            borderColor: 'rgba(255,99,132,1)',
+                            borderWidth: 1,
+                            hoverBackgroundColor: 'rgba(255,99,132,0.4)',
+                            hoverBorderColor: 'rgba(255,99,132,1)',
+                            data: []
+                        }]
+                      }
 
-                //     input.forEach(element => {
+                    input.forEach(element => {
                         
-                //         newData.labels.push(element[0]);
-                //         newData.datasets[0].data.push(element[1]);
-                //     });
+                        newData.labels.push(element[0]);
+                        newData.datasets[0].data.push(element[1]);
+                    });
 
-                //     const newCancellationData = {
-                //         labels: [],
-                //         datasets: [{
-                //             label: 'Transactions Cancelled',
-                //             backgroundColor: ["#765dd9", "#866ee6", "#8f78eb", "#9d88f2", "#a590f5", "#b29ffc", "#bcabff", "#c8baff", "#d4c9ff", "#dad2fc"],
-                //             borderColor: 'rgba(255,99,132,1)',
-                //             borderWidth: 1,
-                //             hoverBackgroundColor: 'rgba(255,99,132,0.4)',
-                //             hoverBorderColor: 'rgba(255,99,132,1)',
-                //             data: []
-                //         }]
-                //       }
-                //     cancelInput.forEach(element => {
+                    const newCancellationData = {
+                        labels: [],
+                        datasets: [{
+                            label: 'Transactions Cancelled',
+                            backgroundColor: ["#765dd9", "#866ee6", "#8f78eb", "#9d88f2", "#a590f5", "#b29ffc", "#bcabff", "#c8baff", "#d4c9ff", "#dad2fc"],
+                            borderColor: 'rgba(255,99,132,1)',
+                            borderWidth: 1,
+                            hoverBackgroundColor: 'rgba(255,99,132,0.4)',
+                            hoverBorderColor: 'rgba(255,99,132,1)',
+                            data: []
+                        }]
+                      }
+                    cancelInput.forEach(element => {
                         
-                //         newCancellationData.labels.push(element[0]);
-                //         newCancellationData.datasets[0].data.push(element[1]);
+                        newCancellationData.labels.push(element[0]);
+                        newCancellationData.datasets[0].data.push(element[1]);
                         
-                //     });
+                    });
 
-                //     setTransactionData(newData);
-                //     setCancellationData(newCancellationData);
-                // //}
+                    setTransactionData(newData);
+                    setCancellationData(newCancellationData);
                 if(prevCounter < 240) {
                     return prevCounter + 1;
                 }
