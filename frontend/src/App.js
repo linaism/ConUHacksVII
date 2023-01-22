27b6448499e4660953cc2930798597b55c838be2
Lina Ismail <ilina19@hotmail.com>
2023-01-21 23:43:12 -0500

    ordered bar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -17,20 +17,26 @@ function test(){
 }
 
 function App() {
-  const [userData, setUserData] = useState({
-    labels: UserData.map((data) => data.year),
+
+
+  const [userData, setUserData] = useState(UserData)
+
+  const sortedData = userData.sort((a, b) => b.gain - a.gain);
+
+  const chartData = {
+    labels: sortedData.map((data) => data.year),
     datasets: [{
       label: "Users Gained",
-      data: UserData.map((data) => data.gain), 
+      data: sortedData.map((data) => data.gain), 
       backgroundColor: ["#765dd9", "#866ee6", "#8f78eb", "#9d88f2", "#a590f5", "#b29ffc", "#bcabff", "#c8baff", "#d4c9ff", "#dad2fc"]
     }]
-  })
+  }
 
   return (
     <div className="App">
       <Clock/>
       <div style={{width: 1000}}>
-        <BarGraph chartData={userData} />
+        <BarGraph chartData={chartData} />
       </div>
     </div>
   );
