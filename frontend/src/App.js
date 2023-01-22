8388cb602644039dd30b215377aacc0ef28a2aac
Abhinav Batra <abhinavbatra2303@gmail.com>
2023-01-21 23:14:29 -0500

    Routes and Front/Back Integration
    Integrating developed backend endpoints with axios

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,5 @@
-import logo from './logo.svg';
+
 import './App.css';
-import ReactSlider from 'react-slider'
 import Slider from './slider';
 import axios from 'axios';
 
@@ -14,6 +13,52 @@ function test(){
   });
 }
 
+// route to return top 10 success per second from all collections
+function getTopTen(){
+  axios.get('http://127.0.0.1:5000/api/top10')
+  .then(response => {
+    console.log(response.data);
+  })
+  .catch(error => {
+    console.log(error);
+  });
+}
+
+// route to return top 10 success overall
+function getTopTenOverall(){
+  axios.get('http://127.0.0.1:5000/api/top10All')
+  .then(response => {
+    console.log(response.data);
+  })
+  .catch(error => {
+    console.log(error);
+  });
+}
+
+//  route to return top 10 success per second from certain collection
+function getTopTenSuccessCertainCollection(){
+  axios.get('http://127.0.0.1:5000/api/TopTenSuccessSeconds/<collection>')
+  .then(response => {
+    console.log(response.data);
+  })
+  .catch(error => {
+    console.log(error);
+  });
+}
+
+// all data from any collection
+function getAllSpecificCollection(collection){
+  axios.get('http://127.0.0.1:5000/api/alldata/$collection')
+  .then(response => {
+    console.log(response.data);
+  })
+  .catch(error => {
+    console.log(error);
+  });
+}
+
+
+
 function App() {
 
   //const [count, setCount] = useState(0);
@@ -25,6 +70,12 @@ function App() {
 
         />
         <button onClick={test}>Press</button>
+        <button onClick={getTopTenOverall}>TopTenOverall</button>
+        <button onClick={getTopTen}>TopTen</button>
+        <button onClick={getAllSpecificCollection("Aequitas")}>AllSpecificCollection</button>
+        <button onClick={getTopTenSuccessCertainCollection("Aequitas")}>Top Ten Success Certain Collection</button>
+
+
 
       </header>
     </div>
